86c83d8348e6faaf12da1000f3338d5fae08f52c
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2022-09-13 07:50:27 +0600

    Initial Commit

---
+++ ErrorHandling/ErrorHandlingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ErrorHandlingApp.swift
+//  ErrorHandling
+//
+//  Created by JOYNAL ABEDIN on 13/9/22.
+//
+
+import SwiftUI
+
+@main
+struct ErrorHandlingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
